88eec2a125cd0ddbfd2b369cdd990289d3656465
SiddhantYerandkar <siddhantyerandkar@gmail.com>
2023-03-18 13:43:28 +0530

    react initialized

---
+++ front-end/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/Home.css"
+
+const Home = () => {
+    return (
+        <>
+            <div>
+                <h1>Book Management</h1>
+            </div>
+        </>
+    )
+}
+
+export default Home
